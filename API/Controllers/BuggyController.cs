35513d455d18b4d9cabe2e3fe6d1c34ce9c0cb2c
Abdelkadous ES-SEHYMY <99757424+AbdelkadousES-SEHYMY@users.noreply.github.com>
2023-08-09 11:58:02 +0100

    Creating a consistent error response from the API

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -1,4 +1,5 @@
-﻿using Infrastructure.Data;
+﻿using API.Errors;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -16,7 +17,7 @@ namespace API.Controllers
         {
             var thing = _context.Products.Find(42);
             if (thing is null)
-                return NotFound();
+                return NotFound(new ApiResponse(400));
             return Ok();
         }
 
