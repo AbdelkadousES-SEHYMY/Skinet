726b4ca77198f261d90babd273629ffd6364e95e
Abdelkadous ES-SEHYMY <99757424+AbdelkadousES-SEHYMY@users.noreply.github.com>
2023-08-09 11:34:45 +0100

    Creating BaseApiController

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -6,9 +6,7 @@ using Core.Specifications;
 using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ProductsController : ControllerBase
+    public class ProductsController : BaseApiController
     {
         private readonly IGenericRepository<Product> _productRepository;
         private readonly IGenericRepository<ProductBrand> _productBrandRepository;
